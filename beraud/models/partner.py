a9580973a0b361a0b8641827ac58610d8090de7f
Baptiste Chaudat <bchaudat@yziact.fr>
2016-04-18 17:57:55 +0200

    [DEL]useless file

--- beraud/models/partner.py
+++
@@ -1,8 +0,0 @@
-from openerp import models, fields
-
-class Partner(models.Model):
-    _inherit = 'res.partner'
-
-    # myCompany_id = fields.Many2one('res.company', "Company", select=1)
-    #company_id = fields.Many2one(required=True)
-
