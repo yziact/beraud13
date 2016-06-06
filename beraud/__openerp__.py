7bcf1975fec229de6e8404420bc01e0e4a62a643
shea <bchaudat@yziact.fr>
2016-06-06 14:53:53 +0200

    [ADD] multiple files for internal_transfer and internal-invoicing

--- beraud/__openerp__.py
+++ beraud/__openerp__.py
@@ -17,9 +17,20 @@
 
     'depends': [
         'base',
+        'product',
+        'stock',
+        'mrp',
+        'project_issue_sheet',
+        'project_issue',
+        'project',
     ],
 
     'data': [
         'views/res_partner.xml',
+        'views/stock_view.xml',
+        'views/transfer_stock_intercompany_view.xml',
+        'views/analytic_line_task.xml',
+        'views/analytic_line_issue.xml',
+        'views/internal_invoice.xml',
     ],
 }
