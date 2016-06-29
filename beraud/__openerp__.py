00b263525ff28f50b51395d511911f463013a185
Fabien Dupuped <fdupuped@yziact.fr>
2016-06-29 10:00:55 +0200

    [FIX] Indentation

--- beraud/__openerp__.py
+++ beraud/__openerp__.py
@@ -34,11 +34,11 @@
         'views/analytic_line_issue.xml',
         'views/internal_invoice.xml',
         'views/sale_view.xml',
-		
-		'report/layout/external_layout_header.xml',
+
+        'report/layout/external_layout_header.xml',
         'report/layout/external_layout_footer.xml',
         'report/repair/report_mrprepairorder.xml',
-		'report/stock/report_picking.xml',
+        'report/stock/report_picking.xml',
         'report/stock/report_shipping2.xml',
         'report/sale/report_sale.xml',
         'report/invoice/report_invoice.xml',
