7597eb954d8b49085e2d4659dae7a7ea39d3aade
Leo Schmitt <lschmitt@yziact.fr>
2016-06-30 10:45:30 +0200

    [FIX] Remove debug prints

--- beraud/controllers/css.py
+++ beraud/controllers/css.py
@@ -18,11 +18,9 @@ from openerp.tools.misc import DEFAULT_SERVER_DATETIME_FORMAT as DTF, ustr
 class CssInterface(http.Controller):
     @http.route(['/beraud/dynamic_backend.css'], type='http')
     def get_css(self, **post):
-        print "_get_css"
         user = http.request.env['res.users'].search([('id','=',request.session.uid)])
 
         if not user:
-            print "not user"
             return ''
 
         else:
@@ -30,7 +28,5 @@ class CssInterface(http.Controller):
             company_border_color = user.company_id.border_color or 'black'
 
             css_data = '.navbar-inverse { background-color: '+ company_background_color + '; border-color: '+ company_border_color + '; }'
-
-            print css_data
 
             return request.make_response(css_data, [('Content-Type', 'text/css')])
