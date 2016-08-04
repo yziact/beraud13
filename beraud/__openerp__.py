e797445d8f4c768222bbac9071e6160132c4446d
Chaudat Baptiste <bchaud@yziact.fr>
2016-08-04 12:21:38 +0200

    [FIX] fix last commit missing init.py openerp.py

---
+++ beraud/__openerp__.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "beraud",
+
+    'summary': """
+        Module beraud """,
+
+    'description': """
+        Module beraud
+    """,
+
+    'author': "Yziact",
+    'website': "http://www.yziact.fr",
+
+    'category': 'Specific Industry Applications',
+    'version': '0.1',
+
+    'depends': [
+        'base',
+        'product',
+        'stock',
+        'mrp',
+        'project_issue_sheet',
+        'project_issue',
+        'project',
+        'product_dimension',
+        'account',
+    ],
+
+    'data': [
+        'views/res_partner.xml',
+        'views/stock_view.xml',
+        'views/company_view.xml',
+        'views/product_view.xml',
+        'views/transfer_stock_intercompany_view.xml',
+        'views/analytic_line_task.xml',
+        'views/analytic_line_issue.xml',
+        'views/internal_invoice.xml',
+        'views/parc_machine.xml',
+        'views/product_category.xml',
+        'views/sale_order.xml',
+
+        'report/layout/external_layout_header.xml',
+        'report/layout/external_layout_footer.xml',
+
+        'report/repair/report_mrprepairorder.xml',
+
+        'report/stock/report_picking.xml',
+        'report/stock/report_shipping2.xml',
+
+        'report/sale/report_sale.xml',
+        'report/sale/sale_stock.xml',
+
+        'report/purchase/report_purchase.xml',
+
+        'report/invoice/report_invoice.xml',
+        'report/invoice/sale_stock.xml',
+    ],
+}
---
+++ beraud/models/__init__.py
@@ -0,0 +1,8 @@
+import logistique
+import transfer_stock_intercompany
+import internal_invoice
+import product
+import partner
+import sale
+import company
+import parc_machine
