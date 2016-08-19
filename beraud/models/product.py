6763c819e39d931ce5d3feb47cf0028ed9e93a5c
Chaudat Baptiste <bchaud@yziact.fr>
2016-08-19 16:03:17 +0200

    [IMP]modification on picking report, add nb_palette for product

--- beraud/models/product.py
+++ beraud/models/product.py
@@ -8,9 +8,11 @@ class ProductTemplate(models.Model):
 
     code_douane = fields.Char('Code douane')
 
-    description_purchase= fields.Html('Purchase Description',translate=True)
-    description_sale = fields.Html('Sale Description',translate=True)
+    description_purchase= fields.Html('Purchase Description', translate=True)
+    description_sale = fields.Html('Sale Description', translate=True)
     description_picking = fields.Html('Picking Description', translate=True)
+
+    nb_palette = fields.Integer('Nombre de palettes')
 
     allee = fields.Char('Allée')
     casier = fields.Char('Casier')
