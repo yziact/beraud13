5a638a9f7a37fbeddcbd6e2f5485460e3c4b6ee1
Chaudat Baptiste <bchaud@yziact.fr>
2016-09-26 16:14:27 +0200

    [ADD] Calendar modul to fix calendar.event

---
+++ calendar/models/calendar.py
@@ -0,0 +1,16 @@
+from openerp import models, fields, api
+
+
+
+class CalendarInherit(models.Model):
+    _inherit = 'calendar.event'
+
+    no_emial = fields.Boolean(default=True)
+
+    @api.model
+    def create(self, vals, context):
+        print vals
+        print context
+        res = super(CalendarInherit, self).create(vals, context)
+
+        return res
