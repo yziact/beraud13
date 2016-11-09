3ad87cde14efae4d939956ec8a9b6bf5236009bc
Chaudat Baptiste <bchaud@yziact.fr>
2016-11-09 14:53:42 +0100

    [FIX]module_facturation: fix _onchange_partner_id() inherit

--- module_facturation/models/invoice.py
+++ module_facturation/models/invoice.py
@@ -8,37 +8,38 @@ class AccountInvoiceInherit(models.Model):
 
     @api.multi
     @api.onchange('partner_id')
-    def onchange_partner_id(self):
+    def _onchange_partner_id(self):
         """
         Update the following fields when the partner is changed:
         - Delivery address
         """
+        super(AccountInvoiceInherit, self)._onchange_partner_id()
 
-        super(AccountInvoiceInherit, self).onchange_partner_id()
+        if self.type in ['out_invoice', 'out_refund']:
+            if not self.partner_id:
+                self.update({
+                    'partner_shipping_id': False,
+                })
+                return
 
-        if not self.partner_id:
-            self.update({
-                'partner_shipping_id': False,
-            })
-            return
+            addr = self.partner_id.address_get(['delivery'])
 
-        addr = self.partner_id.address_get(['delivery'])
-
-        values = {
-            'partner_shipping_id': addr['delivery'],
-        }
-        self.update(values)
+            values = {
+                'partner_shipping_id': addr['delivery'],
+            }
+            self.update(values)
 
     @api.multi
     @api.returns('self')
     def refund(self, date_invoice=None, date=None, description=None, journal_id=None):
         res = super(AccountInvoiceInherit, self).refund(date_invoice, date, description, journal_id)
 
-        if not self.partner_id:
-            res['partner_shipping_id'] = False
-        else :
-            addr = res['partner_id'].address_get(['delivery'])
-            res['partner_shipping_id'] =  addr['delivery']
+        if self.type in ['out_invoice', 'out_refund']:
+            if not self.partner_id:
+                res['partner_shipping_id'] = False
+            else :
+                addr = res['partner_id'].address_get(['delivery'])
+                res['partner_shipping_id'] =  addr['delivery']
 
         return res
 
