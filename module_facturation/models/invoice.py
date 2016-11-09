0ae6e7849333c9fa819c8dceaa4dd4dc51522c46
Chaudat Baptiste <bchaud@yziact.fr>
2016-11-09 11:10:52 +0100

    [IMP]module_facturation: fix field required to false, shipping address on out_refund

--- module_facturation/models/invoice.py
+++ module_facturation/models/invoice.py
@@ -4,8 +4,7 @@ from openerp import models, api, fields
 class AccountInvoiceInherit(models.Model):
     _inherit = 'account.invoice'
 
-    partner_shipping_id = fields.Many2one('res.partner', string='Adresse de Livraison', readonly=True, required=True,
-                                          states={'draft': [('readonly', False)]}, help="Delivery address for current sales order.")
+    partner_shipping_id = fields.Many2one('res.partner', string='Adresse de Livraison', readonly=True, states={'draft': [('readonly', False)]}, help="Delivery address for current sales order.")
 
     @api.multi
     @api.onchange('partner_id')
@@ -14,6 +13,9 @@ class AccountInvoiceInherit(models.Model):
         Update the following fields when the partner is changed:
         - Delivery address
         """
+
+        super(AccountInvoiceInherit, self).onchange_partner_id()
+
         if not self.partner_id:
             self.update({
                 'partner_shipping_id': False,
@@ -26,3 +28,18 @@ class AccountInvoiceInherit(models.Model):
             'partner_shipping_id': addr['delivery'],
         }
         self.update(values)
+
+    @api.multi
+    @api.returns('self')
+    def refund(self, date_invoice=None, date=None, description=None, journal_id=None):
+        res = super(AccountInvoiceInherit, self).refund(date_invoice, date, description, journal_id)
+
+        if not self.partner_id:
+            res['partner_shipping_id'] = False
+        else :
+            addr = res['partner_id'].address_get(['delivery'])
+            res['partner_shipping_id'] =  addr['delivery']
+
+        return res
+
+
