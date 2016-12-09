a17f8ae7aa0396911c06ad061e0ea17307c55f78
Chaudat Baptiste <bchaudat@yziact.fr>
2016-12-09 11:47:00 +0100

    [IMP]module_facturation: modification on the way we gather the salesman

--- module_facturation/models/sale.py
+++ module_facturation/models/sale.py
@@ -14,6 +14,8 @@ class SaleInherit(models.Model):
         res['date_invoice'] = self.last_picking_id_date_done
         res['last_bl_from_bc'] = self.last_picking_id.id
 
+        if self.contact:
+            res['contact'] = self.contact.id
         print res
 
         return res
