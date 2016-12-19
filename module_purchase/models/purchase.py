d47a9a3f24cef0f57e2d072aec5044bbb6f5de59
Chaudat Baptiste <bchaudat@yziact.fr>
2016-12-19 16:27:11 +0100

    [IMP]module_purchase: purchase.py and purchase_view.xml add the commercial and contact, modification on the report_purchase.xml

--- module_purchase/models/purchase.py
+++ module_purchase/models/purchase.py
@@ -14,7 +14,28 @@ _logger = logging.getLogger(__name__)
 
 class PurchaseOrder(models.Model):
     _inherit = 'purchase.order'
+
+    def _default_commercial(self):
+        print "OUR DEFAULT COMMERCIAL"
+        print self.env.context
+        print self.env.user
+
+        if not self.contact_affaire:
+            return self.env.user
+
+    contact_affaire = fields.Many2one('res.users', string='V/Contact affaire', default=_default_commercial)
+    user_id = fields.Many2one('res.users', string='Salesperson', index=True, track_visibility='onchange',
+                              default=lambda self: self.env.user)
+
+
     contact = fields.Many2one('res.partner', readonly=True, states={'draft': [('readonly', False)], 'sent': [('readonly', False)]})
+
+    @api.onchange('partner_id')
+    def onchange_partner_id(self):
+        super(PurchaseOrder, self).onchange_partner_id()
+        if self.partner_id.user_id:
+            self.user_id = self.partner_id.user_id.id
+
 
     @api.model
     def fields_view_get(self, view_id=None, view_type='form', context=None, toolbar=False, submenu=False):
