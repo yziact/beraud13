c83940f03e7fab0ac2982ed824bb4bcf790d9aab
Victor Ravit <vravit@yziact.local>
2018-03-15 16:06:45 +0100

    [NEW] Module: tri des commandes et facturations par date

---
+++ filters_yzi/__openerp__.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Filtres personnalisés",
+
+    'description': """
+    Filtres .""",
+
+    'author': "Yziact",
+    'website': "http://www.yziact.fr",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/master/openerp/addons/base/module/module_data.xml
+    # for the full list
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': [
+        'base',
+        'web',
+        'sale',
+        'account',
+    ],
+
+    # always loaded
+    'data': [
+        'views/filter_order.xml',
+        'views/filter_invoice.xml',
+    ],
+}
+
---
+++ filters_yzi/models/filters.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+
+# [TODOO]: Trier les imports
+
+from openerp import models, api, fields, _
+
+import logging
+_logger = logging.getLogger(__name__)
+
+
+#sys.path.insert(0, '..')
+#sys.path.insert(0, '/var/lib/odoo/odoo-beraud/')
+#sys.path.insert(0, '/var/lib/odoo/odoo-beraud2')
+#sys.path.insert(0, '/mnt/extra-addons/')
+
+
+
+
